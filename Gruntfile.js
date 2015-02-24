7877ec44af1fe5b2182ff2400ec95f49c860105e
Javier Cejudo <javier@javiercejudo.com>
2015-02-25 01:46:31 +1100

    feat(dist): upload dist to S3

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,26 @@ module.exports = function(grunt) {
         files: 'archivos_memoria/**/*.tex',
         tasks: ['default']
       }
+    },
+    aws_s3: {
+      pdf: {
+        options: {
+          accessKeyId: process.env.S3_KEY,
+          secretAccessKey: process.env.S3_SECRET,
+          bucket: 'proyecto-fin-carrera',
+          differential: true
+        },
+        files: [
+          {expand: true, cwd: 'dist', src: ['**'], dest: grunt.template.today('isoUtcDateTime')},
+        ]
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-latex');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-aws-s3');
 
   // Default task. Runs latex twice to get cross-references and indexes right
   grunt.registerTask('default', ['latex:pdf', 'latex:pdf']);
